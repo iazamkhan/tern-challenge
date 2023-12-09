74b84f3dffe33e9c90e7cf9735afb7cbd35bc074
Azam <azamkhan561@gmail.com>
2023-12-09 19:06:48 +0530

    able to load data in dropdown on first load

--- src/JobsDropdown.jsx
+++ src/JobsDropdown.jsx
@@ -3,25 +3,48 @@ import axios from 'axios'
 
 function JobjsDropdown(props) {
     const [data, setData] = useState([])
-    const [options, setOptions] = useState(<></>)
+    const [optValue, setOptValue] = useState("")
 
     useEffect(() => {
        axios.get("https://api.adzuna.com/v1/api/jobs/in/categories?app_id=f7945ab6&app_key=b8aad504c1a95de5f8177b7fc6d25948")
-       .then(res => console.log(res?.data.results))
        .then(res => setData(res?.data.results))
+       .then(console.log(data))
     }, [])
 
-    useEffect(() => {
-            setOptions(data?.map(item => (<option value={item.tag}>{item.label}</option>)))
-    }, [data])
-    // const options = (<><option value="first">First</option><option value="second">Second</option></>)
+    const handleChange = (e) => {
+        setOptValue(e.target.value)
+    }
+
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const {data} = await axios.get("https://api.adzuna.com/v1/api/jobs/in/categories?app_id=f7945ab6&app_key=b8aad504c1a95de5f8177b7fc6d25948");
+    //         const results = [];
+    //         data.forEach((value) => {
+    //             results.push({
+    //                 key: value.tag,
+    //                 value: value.label
+    //             })
+    //         })
+    // setOptions([{key: 'Select a company', value: ''},
+    // ...results])
+    //     }
+    //     fetchData();
+    // }, [])
 
     return (
         <>
             <label for="options" style={{marginRight: '4px'}}>Select category</label>
-            <select>
-                {options}
+            <select onChange={handleChange} value={optValue}>
+                {data ? (
+                    data.map((item) => {
+                        return (
+                            <option value={item.label}>{item.tag}</option>
+                        )
+                    })
+                ) : (<></>)}
             </select>
+            {optValue ? (<p>{optValue}</p>) : (<></>)}
         </>
     )
 }
